21cad2854f78f7b4a193a67f892b0f169ea97c61
krystallinekim <jskim11070@gmail.com>
2025-09-19 12:39:10 +0900

    prompt_listing rename

--- program/models/node_regenerate.py
+++ program/models/node_regenerate.py
@@ -1,6 +1,6 @@
 from schemas.global_state import State
 from schemas.schema import TitleOutput, BPOutput, DescriptionOutput
-from prompts.prompt_listing import title_prompt, bp_prompt, description_prompt
+from program.prompts.prompt_listing import title_prompt, bp_prompt, description_prompt
 from langchain_openai import ChatOpenAI
 from utils.config_loader import config
 from dotenv import load_dotenv
